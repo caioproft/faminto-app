5540f387c5857a3e3ae17ad395b20bd2c715abe2
caioproft <caio_proft@hotmail.com>
2020-12-07 21:26:27 -0300

    Projeto criado

---
+++ src/main/java/br/com/crs/faminto/FamintoApplication.java
@@ -0,0 +1,13 @@
+package br.com.crs.faminto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamintoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamintoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/crs/faminto/FamintoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crs.faminto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamintoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
